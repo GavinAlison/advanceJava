a6466abcea32d1efad7e6f6c320d95dab97d6d37
Gavin.Alison <921757697@qq.com>
2019-09-03 23:52:59 +0800

    command

--- case-redis/case-str/src/test/java/com/alison/command/BaseConnection.java
+++ case-redis/case-str/src/test/java/com/alison/command/BaseConnection.java
@@ -1,4 +1,72 @@
 package com.alison.command;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BaseConnection {
+    protected static Jedis jedis = null;
+
+    protected RedisTemplate redisTemplate;
+
+    protected ValueOperations valueOperations;
+
+    protected HashOperations hashOperations;
+
+    protected ListOperations listOperations;
+
+    protected SetOperations setOperations;
+
+    protected ZSetOperations zSetOperations;
+
+    protected GeoOperations geoOperations;
+
+    protected HyperLogLogOperations hyperLogLogOperations;
+
+    @Resource
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.valueOperations = redisTemplate.opsForValue();
+        this.hashOperations = redisTemplate.opsForHash();
+        this.listOperations = redisTemplate.opsForList();
+        this.setOperations = redisTemplate.opsForSet();
+        this.zSetOperations = redisTemplate.opsForZSet();
+        this.geoOperations = redisTemplate.opsForGeo();
+        this.hyperLogLogOperations = redisTemplate.opsForHyperLogLog();
+
+    }
+
+    /**
+     * 连接
+     */
+    @BeforeClass
+    public static void init() {
+        jedis = new Jedis("192.168.56.103", 6379);
+        jedis.auth("redis");
+        System.out.println("connected......");
+    }
+
+    /**
+     * 关闭连接
+     */
+    @AfterClass
+    public static void destroy() {
+        jedis.flushDB();
+        jedis.close();
+        jedis = null;
+    }
+
+    @Test
+    public void test() {
+        System.out.println("hahaha");
+    }
 }
--- case-redis/case-str/src/test/java/com/alison/command/connect/ConnectionCommand.java
+++ case-redis/case-str/src/test/java/com/alison/command/connect/ConnectionCommand.java
@@ -1,4 +1,84 @@
 package com.alison.command.connect;
 
-public class ConnectionCommand {
+import com.alison.command.BaseConnection;
+import org.junit.Test;
+
+public class ConnectionCommand extends BaseConnection {
+    /**
+     * 请求在受密码保护的 Redis 服务器中进行身份验证。
+     * AUTH password
+     * 返回值：RESP Simple Strings 详见：https://redis.io/topics/protocol#simple-string-reply
+     * 命令：
+     * auth 123456
+     */
+    @Test
+    public void auth() {
+        jedis.keys("*");//在需要密码的redis中会返回错误：(error) NOAUTH Authentication required.
+        jedis.auth("redis");
+    }
+
+    /**
+     * 回显消息
+     * ECHO message
+     * 返回值：message
+     * 命令：
+     * echo message
+     */
+    @Test
+    public void echo() {
+        System.out.println(jedis.echo("message"));
+    }
+
+    /**
+     * 此命令通常用于测试连接是否仍然存在，或测量延迟。
+     * PING [message]
+     * 返回值：返回PONG如果没有提供参数，否则返回参数的副本作为一个主体。
+     * 命令：
+     * ping
+     * ping hello
+     */
+    @Test
+    public void ping() {
+        System.out.println("jedis执行结果：" + jedis.ping());
+        //spring
+        System.out.println("redisTemplate执行结果：" + redisTemplate.getConnectionFactory().getConnection().ping());
+    }
+
+    /**
+     * 请求服务器关闭连接。
+     * OK
+     * 返回值：OK
+     * 命令：  quit
+     */
+    @Test
+    public void quit() {
+        jedis.quit();
+    }
+    /**
+     * 选择Redis 逻辑数据库。新连接始终使用数据库0。
+     * SELECT index
+     * 返回值：
+     * 命令：
+     * select 1
+     * select 0
+     */
+    @Test
+    public void select() {
+        jedis.select(1);
+        jedis.select(0);
+        redisTemplate.getConnectionFactory().getConnection().select(1);
+    }
+
+    /**
+     * 交换两个 Redis 数据库
+     * SWAPDB index index
+     * 返回值：成功返回OK
+     * 命令：
+     * swapdb 0 1
+     * swapdb 1 0
+     */
+    @Test
+    public void swapdb() {
+
+    }
 }
