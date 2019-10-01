b153288d5fa6a19df5f3a3e8803b300980b0409a
alison <921757697@qq.com>
2019-10-01 17:10:18 +0800

    add header

--- learnrabbitmq/src/main/java/com/alison/source/Headers.java
+++ learnrabbitmq/src/main/java/com/alison/source/Headers.java
@@ -5,64 +5,64 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class Headers {
 
-    private static final String EXCHANGE_NAME = "header_test";
+    private static final String EXCHANGE_NAME = "header_exchange";
 
     @Test
     public void emitLogHeader() throws IOException {
 
         String message = "message";
         String routingKey = "ourTestRoutingKey";
-        Map<String, Object> headers = new HashMap<>();
+
         Connection connection = ConnectionUtil.getRabbitConnection();
         Channel channel = connection.createChannel();
+
         channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.HEADERS);
 
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("aaa", "1234");
         AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder();
-
-        builder.deliveryMode(MessageProperties.PERSISTENT_TEXT_PLAIN.getDeliveryMode());
-        builder.priority(MessageProperties.PERSISTENT_TEXT_PLAIN.getPriority());
-
         builder.headers(headers);
 
         AMQP.BasicProperties theProps = builder.build();
 
         channel.basicPublish(EXCHANGE_NAME, routingKey, theProps, message.getBytes("UTF-8"));
-        System.out.println(" 发送信息:message: '" + message + "'");
+        System.out.println(" send:message: '" + message + "'");
     }
 
     @Test
     public void receiveLogHeader() throws IOException {
-        String[] argv = new String[2];
-        argv[0] = "ourTestRoutingKey";
-
         Connection connection = ConnectionUtil.getRabbitConnection();
         Channel channel = connection.createChannel();
 
+        String routingKey = "ourTestRoutingKey";
+        String QUEUE_NAME = "header-queue";
+
+        //声明转发器和类型headers
         channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.HEADERS);
-        String routingKeyFromUser = "ourTestRoutingKey";
+        channel.queueDeclare(QUEUE_NAME, false, false, true, null);
 
-        String queueInputName = argv[0];
-        Map<String, Object> headers = new HashMap<String, Object>();
-//        headers.put(argv[0], argv[1]);
-//        System.out.println("Binding header " + argv[0] + " and value " + argv[1] + " to queue " + queueInputName);
+        Map<String, Object> headers = new Hashtable<String, Object>();
+        headers.put("x-match", "any");//all any
+        headers.put("aaa", "01234");
+        headers.put("bbb", "56789");
+        // 为转发器指定队列，设置binding 绑定header键值对
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, routingKey, headers);
 
-        String queueName = channel.queueDeclare(queueInputName, true, false, false, null).getQueue();
-        channel.queueBind(queueName, EXCHANGE_NAME, routingKeyFromUser, headers);
-
-        System.out.println(" 等待信息中...");
+        System.out.println(" waiting...");
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
-                System.out.println(" 接收到信息:. '" + envelope.getRoutingKey() + "':'" + message + "'");
+                System.out.println(" receive message:. '" + envelope.getRoutingKey() + "':'" + message + "'");
             }
         };
-        channel.basicConsume(queueName, true, consumer);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
     }
 
 }
